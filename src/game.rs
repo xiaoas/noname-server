161c25160a8b8f65cc1c503e3f7fe6717458029c
xiaoqilin <xiaoqilin@bytedance.com>
2022-03-11 11:31:40 +0800

    refine modules

--- src/game.rs
+++ src/game.rs
@@ -1,24 +1,48 @@
-use std::sync::{Arc, Weak};
-
+use std::{sync::{Arc, Weak}, collections::{HashSet, HashMap}};
+use serde::{Serialize, Deserialize};
 use tokio::sync::{mpsc, RwLock};
-
 use salvo::extra::ws::Message;
 
 pub struct Client {
-    tx: mpsc::UnboundedSender<Result<Message, salvo::Error>>,
-    nick_name: String,
-    avatar: String,
+    pub tx: mpsc::UnboundedSender<Result<Message, salvo::Error>>,
+    pub nick_name: String,
+    pub avatar: String,
+    pub status: String,
     room: JoinedRoom,
 }
 pub enum JoinedRoom { // implement -> Option<RwLock<Room>> ?
-    Guest(Weak<RwLock<Room>>),
-    Owner(Arc<RwLock<Room>>)
+    None,
+    Guest(String),
+    Owner(Room)
 }
-pub struct Room {
-
-}
-impl Drop for Room {
-    fn drop(&mut self) {
-        todo!()
+impl Client {
+    pub async fn room(&self, clients: &HashMap<String, Client>) -> Option<&Room> {
+        match &self.room {
+            JoinedRoom::None => None,
+            JoinedRoom::Guest(owner_key) => {
+                let clients = clients;
+                let joined_room = &clients.get(owner_key)?.room;
+                if let JoinedRoom::Owner(room) = joined_room {
+                    Some(room)
+                } else {
+                    None
+                }
+            }
+            JoinedRoom::Owner(room) => Some(&room)
+        }
     }
+}
+#[derive(Serialize, Deserialize)]
+pub struct Room {
+    pub config: RoomConfig,
+    // maybe not
+    // members: HashSet<String>, 
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RoomConfig {
+    pub game_started: bool,
+    pub observe: bool,
+    pub observe_ready: bool,
 }
